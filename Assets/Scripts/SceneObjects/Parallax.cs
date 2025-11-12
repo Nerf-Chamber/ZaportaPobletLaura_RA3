ad5fcfc4051d30e383fc6362134dc9c9375a1aa1
Nerf-Chamber <lzpobletpobletos@gmail.com>
2025-11-12 18:28:44 +0100

    Parallax working!

--- Assets/Scripts/SceneObjects/Parallax.cs
+++ Assets/Scripts/SceneObjects/Parallax.cs
@@ -2,70 +2,30 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    Transform cam;
-    Vector3 startPos;
-    float distance;
+    private float startPos, length;
+    public GameObject player;
+    public float parallaxEffect;
 
-    GameObject[] backgrounds;
-    Material[] mat;
-    float[] backSpeed;
+    public float smoothTime = 0.3f;
+    private float velocity = 0f;
 
-    float farthestBack;
-
-    [Range(0.01f, 1f)]
-    public float parallaxSpeed = 0.5f;
-
-    void Start()
+    private void Start()
     {
-        cam = Camera.main.transform;
-        startPos = player.transform.position;
-
-        int backCount = transform.childCount;
-        mat = new Material[backCount];
-        backSpeed = new float[backCount];
-        backgrounds = new GameObject[backCount];
-
-        for (int i = 0; i < backCount; i++)
-        {
-            backgrounds[i] = transform.GetChild(i).gameObject;
-            mat[i] = backgrounds[i].GetComponent<Renderer>().material;
-        }
-
-        BackSpeedCalculate(backCount);
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-
-    void BackSpeedCalculate(int backCount)
+    private void FixedUpdate()
     {
-        for (int i = 0; i < backCount; i++)
-        {
-            if ((backgrounds[i].transform.position.z - cam.position.z) > farthestBack)
-            {
-                farthestBack = backgrounds[i].transform.position.z - cam.position.z;
-            }
-        }
+        float targetX = startPos + player.transform.position.x * parallaxEffect;
+        float smoothX = Mathf.SmoothDamp(transform.position.x, targetX, ref velocity, smoothTime);
 
-        for (int i = 0; i < backCount; i++)
-        {
-            backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
-        }
-    }
+        transform.position = new Vector3(smoothX, transform.position.y, transform.position.z);
 
-    private void LateUpdate()
-    {
-        distance = player.transform.position.x - startPos.x;
+        float movement = player.transform.position.x * (1 - parallaxEffect);
 
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            float speed = backSpeed[i] * parallaxSpeed;
-            mat[i].SetTextureOffset("_MainTex", new Vector2(distance, 0) * speed);
-        }
-    }
-
-    private void Update()
-    {
-        transform.position = new Vector2(transform.position.x, cam.position.y);
+        if (movement > startPos + length)
+            startPos += length;
+        else if (movement < startPos - length)
+            startPos -= length;
     }
 }
