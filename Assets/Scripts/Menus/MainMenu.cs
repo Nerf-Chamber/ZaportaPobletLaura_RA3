698a1087fc104db43ff63c5b67b2377e1971dbc0
Nerf-Chamber <lzpobletpobletos@gmail.com>
2025-11-21 14:26:31 +0100

    CameraChanges bug fixed and Camera System simplified

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,11 +5,4 @@ public class MainMenu : BaseMenu
     public static bool GameStarted = false;
 
     protected override void Awake() => MenuUI.SetActive(true);
-
-    // Works as the start
-    public override void Restart()
-    {
-        base.Restart();
-        GameStarted = true;
-    }
 }
--- Assets/Scripts/SceneObjects/Player.cs
+++ Assets/Scripts/SceneObjects/Player.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,14 +6,15 @@ public class Player : Character, InputSystem_Actions.IPlayerActions
 {
     private AudioClip clip;
     private InputSystem_Actions inputActions;
-    private Dictionary<(CameraLocations, CameraLocations), bool> cameraChangeStates;
-    private bool canCameraChange = true;
     private bool enteredFinalZone = false;
 
     public Vector2 initialPosition;
 
     public static bool isDead = false;
     public static bool didWin = false;
+
+    public float limitChangeCameraX;
+    public float limitChangeCameraY;
 
     private bool canCollectCoins = true;
     public int coinsCollected = 0;
@@ -44,25 +44,27 @@ public class Player : Character, InputSystem_Actions.IPlayerActions
     private void OnEnable() => inputActions.Enable();
     private void OnDisable() => inputActions.Disable();
 
-    private void Start()
-    {
-        cameraChangeStates = new Dictionary<(CameraLocations, CameraLocations), bool>
-        {
-            {(CameraLocations.StageOne, CameraLocations.StageTwo), false},
-            {(CameraLocations.StageTwo, CameraLocations.StageThree), false},
-            {(CameraLocations.StageThree, CameraLocations.StageFour), false},
-            {(CameraLocations.StageFour, CameraLocations.StageOne), false}
-        };
-    }
-
     private void Update()
     {
+        // Movement related
         isMovingX = onMoveDirection.x != 0;
 
         _move.MoveX(onMoveDirection.x, speed);
         if (!isFacingRight && onMoveDirection.x > 0) { _animation.FlipX(ref isFacingRight); }
         else if (isFacingRight && onMoveDirection.x < 0) { _animation.FlipX(ref isFacingRight); }
         _animation.SetRunState(isMovingX);
+
+        // Camera related
+        if (transform.position.x > limitChangeCameraX)
+        {
+            if (transform.position.y > limitChangeCameraY) CameraManager.Instance.ChangeStage(CameraLocations.StageFour);
+            else CameraManager.Instance.ChangeStage(CameraLocations.StageThree);
+        }
+        else
+        {
+            if (transform.position.y > limitChangeCameraY) CameraManager.Instance.ChangeStage(CameraLocations.StageOne);
+            else CameraManager.Instance.ChangeStage(CameraLocations.StageTwo);
+        }
     }
 
     // ----- MOVEMENT -----
@@ -90,7 +92,6 @@ public class Player : Character, InputSystem_Actions.IPlayerActions
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        CameraChangesCollision(collision);
         CollectableCollision(collision);
 
         if (collision.gameObject.layer == LayerMask.NameToLayer("FinalZone") && !enteredFinalZone)
@@ -103,53 +104,6 @@ public class Player : Character, InputSystem_Actions.IPlayerActions
     {
         if (coinsCollected == 8) { Win(); }
         else { LooseAsASoup(); }
-    }
-    private void CameraChangesCollision(Collider2D collision)
-    {
-        if (!canCameraChange) return;
-
-        (CameraLocations, CameraLocations) locations;
-
-        switch (collision.gameObject.layer)
-        {
-            case int layer when layer == LayerMask.NameToLayer("CameraChangeOne"):
-                locations = (CameraLocations.StageOne, CameraLocations.StageTwo);
-                break;
-            case int layer when layer == LayerMask.NameToLayer("CameraChangeTwo"):
-                locations = (CameraLocations.StageTwo, CameraLocations.StageThree);
-                break;
-            case int layer when layer == LayerMask.NameToLayer("CameraChangeThree"):
-                locations = (CameraLocations.StageThree, CameraLocations.StageFour);
-                break;
-            case int layer when layer == LayerMask.NameToLayer("CameraChangeFour"):
-                locations = (CameraLocations.StageFour, CameraLocations.StageOne);
-                break;
-            default:
-                return;
-        }
-
-        if (cameraChangeStates.TryGetValue(locations, out bool didCameraChange))
-        {
-            ChangeCamera(locations, ref didCameraChange);
-            cameraChangeStates[locations] = didCameraChange;
-        }
-    }
-    // IEnumerator type return for coroutine
-    private IEnumerator CameraChangeCooldown(float delay)
-    {
-        canCameraChange = false;
-        // Yield: Returns one value at a time
-        yield return new WaitForSeconds(delay);
-        canCameraChange = true;
-    }
-    private void ChangeCamera((CameraLocations, CameraLocations) locations, ref bool didCameraChange)
-    {
-        CameraLocations locationToGo = didCameraChange ? locations.Item1 : locations.Item2;
-        CameraManager.Instance.ChangeStage(locationToGo);
-        didCameraChange = !didCameraChange;
-
-        // Coroutine: Special type of function that can be paused and resumed later. Concurrency and multitasking :D
-        StartCoroutine(CameraChangeCooldown(0.5f));
     }
 
     private void CollectableCollision(Collider2D collision)
@@ -230,10 +184,5 @@ public class Player : Character, InputSystem_Actions.IPlayerActions
         coinsCollected = 0;
         ResetCameraValues();
     }
-    private void ResetCameraValues()
-    {
-        var keys = new List<(CameraLocations, CameraLocations)>(cameraChangeStates.Keys);
-        foreach (var key in keys) { cameraChangeStates[key] = false; }
-        CameraManager.Instance.ChangeStage(CameraLocations.StageOne);
-    }
+    private void ResetCameraValues() => CameraManager.Instance.ChangeStage(CameraLocations.StageOne);
 }
